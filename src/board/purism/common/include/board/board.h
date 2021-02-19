6d85dfd047d81465b304bb174a21c1fb91189fc5
Nicole Faerber <nicole.faerber@puri.sm>
2021-02-19 16:05:39 +0100

    add some documentation

--- src/board/purism/common/include/board/board.h
+++ src/board/purism/common/include/board/board.h
@@ -7,11 +7,20 @@
 
 void board_init(void);
 void board_event(void);
+void board_1s_event(void);
 void board_on_ac(bool ac);
 
 void board_battery_init(void);
+
+// true - battery present
+// flase - battery not present
 bool board_battery_detect(void);
+
+// voltage in mV
 uint16_t board_battery_get_voltage(void);
-uint16_t board_get_battery_charge(void);
+// current in mA
+uint16_t board_battery_get_current(void);
+// charge in % (0 to 100)
+uint16_t board_battery_get_charge(void);
 
 #endif // _BOARD_BOARD_H
