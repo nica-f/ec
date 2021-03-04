a2dcb4537bbcb62ae3821870d338b3cd796a5450
Nicole Faerber <nicole.faerber@puri.sm>
2021-03-04 16:16:16 +0100

    improve (fix) charge disabling

--- src/board/purism/common/charger/bq24715.c
+++ src/board/purism/common/charger/bq24715.c
@@ -8,19 +8,63 @@
 #include <common/debug.h>
 
 // ChargeOption0 flags
-// Low Power Mode Enable
-#define SBC_EN_LWPWR        ((uint16_t)(1 << 15))
-// Watchdog Timer Adjust
-#define SBC_WDTMR_ADJ_175S  ((uint16_t)(0b11 << 13))
+// ChargeInhibit
+// 1=inihibt charge
+#define SBC_CHARGE_INHIBIT  ((uint16_t)(1 << 0))
+// 1=enable
+#define SBC_IDPM_EN         ((uint16_t)(1 << 1))
+// 1=enable, precharge clamped below 384mA
+#define SBC_LDO_MODE_EN     ((uint16_t)(1 << 2))
+// 0: switch IOUT based on IDPM_EN <default @ POR>
+// 1: select IOUT based on bit[4]
+#define SBC_FIX_IOUT        ((uint16_t)(1 << 3))
+// When bit[3]=1, bit[4] serve as input
+// 0: IOUT is the 40x adapter current amplifier output
+// 1: IOUT is the 16x discharge current amplifier output
+// When bit[3]=0, bit[4] serve as output (indicate IOUT selection)
+// 0: IOUT the 40x adapter current amplifier output; when IDPM is disabled <default @ POR>
+// 1: IOUT is the 16x discharge current amplifier output; when IDPM is enabled
+#define SBC_IOUT_SEL        ((uint16_t)(1 << 4))
+// 0: Disable LEARN Mode <default @ POR>
+// 1: Enable LEARN Mode
+#define SBC_LEARN_EN        ((uint16_t)(1 << 5))
+// 0: 250mV
+// 1: 350mV <default>
+#define SBC_LSFET_OCP_THR   ((uint16_t)(1 << 6))
+// 0: function is disable <default @ POR>
+// 1: 333% IDPM
+#define SBC_ACOC            ((uint16_t)(1 << 7))
 // Switching Frequency
+// 00:600kHz
+// 01:800kHz <default @ POR>
+// 10: 1MHz
+//11: 800 kHz
 #define SBC_PWM_FREQ_800KHZ ((uint16_t)(0b01 << 8))
 // AudioFrequencyLimit
+// 0: No limit of switching frequency <default @ POR>
+// 1: Set minimum switching frequency to 40 kHz to avoid audio noise
 #define SBC_AUDIO_FREQ_LIM  ((uint16_t)(1 << 10))
-// ChargeInhibit
-#define SBC_CHARGE_INHIBIT  ((uint16_t)(1 << 0))
+// 0: not OVP (default; write 0 to clear the OVP status)
+// 1: OVP latch (read only)
+#define SBC_SYSOVP_STAT     ((uint16_t)(1 << 11))
+//Converter latch-off when sysovp is detected.
+// 0: 15.1 V for 3s; 10.1 V for 2s <default @ POR>
+// 1: 17.0 V for 3s, 11.3 V for 2s
+#define SBC_SYSOVP          ((uint16_t)(1 << 12))
+// Watchdog Timer Adjust
+// 00: Disable Watchdog Timer
+// 01: Enabled, 44 sec
+// 10: Enabled, 88 sec
+// 11: Enable Watchdog Timer (175s) <default @ POR>
+#define SBC_WDTMR_ADJ_175S  ((uint16_t)(0b11 << 13))
+// Low Power Mode Enable
+// Effective on BAT power only (ACDET<0.6 or VCC<UVLO)
+// 0: turn on discharge current monitoring
+// 1: lower quiescent current, discharge current monitoring are turned off <default @ POR>
+#define SBC_EN_LWPWR        ((uint16_t)(1 << 15))
 
-// XXX: Assumption: ac_last is initialized high.
-static bool charger_enabled = false;
+
+static bool charger_enabled = true; // charger is enable by default at POR
 
 int battery_charger_disable(void) {
     int res = 0;
@@ -30,24 +74,9 @@ int battery_charger_disable(void) {
 
     DEBUG("CHG disable\n");
 
-    // Set charge option 0 with 175s watchdog
-    res = smbus_write(
-        CHARGER_ADDRESS,
-        0x12,
-        /* SBC_EN_LWPWR |*/	// if _not_ set discharge current is monitored as well
-        SBC_WDTMR_ADJ_175S |
-        SBC_PWM_FREQ_800KHZ |
-        SBC_AUDIO_FREQ_LIM |
-        SBC_CHARGE_INHIBIT
-    );
-#if 1
+#if 0
     // Disable charge voltage
     res = smbus_write(CHARGER_ADDRESS, 0x15, 0);
-    if (res < 0)
-        return res;
-
-    // Disable charge current
-    res = smbus_write(CHARGER_ADDRESS, 0x14, 0);
     if (res < 0)
         return res;
 
@@ -55,7 +84,20 @@ int battery_charger_disable(void) {
     res = smbus_write(CHARGER_ADDRESS, 0x3F, 0);
     if (res < 0)
         return res;
+
+    // Disable charge current
+    res = smbus_write(CHARGER_ADDRESS, 0x14, 0);
+    if (res < 0)
+        return res;
 #endif
+
+    // Set charge option 0 with 175s watchdog
+    res = smbus_write(
+        CHARGER_ADDRESS,
+        0x12,
+        // SBC_CHARGE_INHIBIT | SBC_LDO_MODE_EN | SBC_LSFET_OCP_THR | SBC_PWM_FREQ_800KHZ | SBC_WDTMR_ADJ_175S
+        SBC_CHARGE_INHIBIT | SBC_IDPM_EN | SBC_LDO_MODE_EN | SBC_LSFET_OCP_THR | SBC_PWM_FREQ_800KHZ | SBC_WDTMR_ADJ_175S
+    );
     DEBUG("CHG disabled\n");
     charger_enabled = false;
 
@@ -98,20 +140,15 @@ int battery_charger_enable(void) {
     res = smbus_write(
         CHARGER_ADDRESS,
         0x12,
-        /* SBC_EN_LWPWR |*/
         SBC_PWM_FREQ_800KHZ |
-        SBC_AUDIO_FREQ_LIM /*|
-        SBC_CHARGE_INHIBIT*/ // XXX for testing disabled
+        SBC_IDPM_EN |
+        SBC_FIX_IOUT |
+        SBC_AUDIO_FREQ_LIM
     );
 
     DEBUG("CHG enabled\n");
     charger_enabled = true;
     return 0;
-}
-
-void battery_charger_event(void) {
-    //TODO: watchdog
-    //DEBUG("CHG event\n");
 }
 
 void battery_debug(void) {
@@ -149,3 +186,10 @@ void battery_debug(void) {
 
     #undef command
 }
+
+void battery_charger_event(void) {
+    //TODO: watchdog
+    //DEBUG("CHG event\n");
+    //battery_debug();
+}
+
